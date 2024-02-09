0d100d1b3f1e04009ae77729bdb13bbce80d2e21
yuvalTrip <77538019+yuvalTrip@users.noreply.github.com>
2024-02-09 15:17:07 +0200

    DL model in process

--- Preceptron.py
+++ Preceptron.py
@@ -1,43 +1,36 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 def perceptron():
-    features = 15
-    hidden_layer_nodes = 10
-    x = tf.keras.Input(shape=(features,), dtype=tf.float64)
-    y_ = tf.keras.Input(shape=(1,), dtype=tf.float64)
+    # Load data from CSV
+    data = pd.read_csv('heart-disease-data.csv')
+    features = data.iloc[:, :-1]  # Select all columns except the last one as features
+    labels = data.iloc[:, -1]     # Select the last column as labels
 
-    W1 = tf.Variable(tf.cast(tf.random.truncated_normal([features, hidden_layer_nodes], stddev=0.1), dtype=tf.float64))
-    b1 = tf.Variable(tf.constant(0.1, shape=[hidden_layer_nodes], dtype=tf.float64))
+    # Convert data to numpy arrays
+    data_x = features.values
+    data_y = labels.values.reshape(-1, 1)  # Reshape labels to match model output shape
 
-    #z1 = tf.add(tf.matmul(x, W1), b1)###
+    # Define model architecture
+    features_count = data_x.shape[1]
+    hidden_layer_nodes = 15
 
-    first=tf.cast(tf.matmul(x, W1), tf.float64)
-    z1 = tf.add(first, b1)  # Cast the result of matmul to tf.float64
+    inputs = tf.keras.Input(shape=(features_count,), dtype=tf.float64)
+    x = tf.keras.layers.Dense(hidden_layer_nodes, activation='relu')(inputs)
+    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-    a1 = tf.nn.relu(z1)
+    # Compile the model
+    model.compile(optimizer='sgd', loss='binary_crossentropy')
 
-    W2 = tf.Variable(tf.cast(tf.random.truncated_normal([hidden_layer_nodes, 1], stddev=0.1), dtype=tf.float64))
-    b2 = tf.Variable(0.)
-    z2 = tf.matmul(a1, W2) + b2
-    pred = 1 / (1.0 + tf.exp(-z2))
-
-    loss = tf.reduce_mean(-(y_ * tf.math.log(pred) + (1 - y_) * tf.math.log(1 - pred)))
-    optimizer = tf.keras.optimizers.SGD(learning_rate=0.0001)
-
-    @tf.function
-    def train_step(inputs, targets):
-        with tf.GradientTape() as tape:
-            predictions = 1 / (1.0 + tf.exp(-tf.matmul(inputs, W1) + b1))
-            current_loss = tf.reduce_mean(-(targets * tf.math.log(predictions) + (1 - targets) * tf.math.log(1 - predictions)))
-        gradients = tape.gradient(current_loss, [W1, b1, W2, b2])
-        optimizer.apply_gradients(zip(gradients, [W1, b1, W2, b2]))
-
-    data_x = np.random.rand(100, features)
-    data_y = np.random.randint(2, size=(100, 1))
-
-    for i in range(50000):
-        train_step(data_x, data_y)
+    # Train the model
+    """we started with 10 epoches and get 
+    The ultimate epochs number we arrive was 80 with loss of 0.4261, but we saw it converge from epoch 23 to this value.
+    
+    
+    """
+    model.fit(data_x, data_y, epochs=10, batch_size=32)
 
 if __name__ == "__main__":
     perceptron()
